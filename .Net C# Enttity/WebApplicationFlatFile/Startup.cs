b23236a727629f8417c321d2f03730800b146ce1
SneyderVargas <sneyr845@gmail.com>
2023-02-06 14:28:44 -0500

    create startup controll api

--- .Net C# Enttity/WebApplicationFlatFile/Startup.cs
+++ .Net C# Enttity/WebApplicationFlatFile/Startup.cs
@@ -2,5 +2,30 @@
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors();
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", "nosniff");
+                await next();
+            });
+            app.UseCors(
+                option =>
+                option.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyMethod()
+                );
+        }
     }
 }
