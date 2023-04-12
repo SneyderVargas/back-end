77ed950e0d1353bdf458a500fd48d9a0ee6afaa1
Jhonathan Vargas <sneyr845@gmail.com>
2023-04-12 16:42:05 -0500

    configuration controller tenancy

--- .Net C# Enttity/WebApplicationFlatFile/Controllers/AccountController.cs
+++ .Net C# Enttity/WebApplicationFlatFile/Controllers/AccountController.cs
@@ -54,7 +54,6 @@ namespace AccountControll.Controllers
             //public async Task<IActionResult> CreateRole([FromBody] CreateRoleRequestDto request, CancellationToken ct)
             //if (!ModelState.IsValid)
             //    return BadRequest(new ApiErrorDto(ModelState));
-
             try
             {
                 var (Succeeded, Message, Users) = await _accountServices.GetAllAdmUsers();
@@ -75,50 +74,6 @@ namespace AccountControll.Controllers
                 var err = ex;
                 return Ok(ApiCollectionResponseDto.CreateOk(err.Message, null, 0));
             }           
-
-        }
-        [HttpPost]
-        public async Task<IActionResult> CreateTenancys([FromBody] CreateAdmTenancysRequest param)
-        {            
-            try
-            {
-                AdmTenancysEntity data = _mapper.Map<AdmTenancysEntity>(param);
-                if (!ModelState.IsValid)
-                    return BadRequest(new ApiErrorDto(ModelState));
-                var (Succeeded, Message) = await _tenancyServices.Create(data);
-                return Ok(ApiCollectionResponseDto.CreateOk(Message, null, 0));
-            }
-            catch(Exception ex)
-            {
-                var err = ex;
-                return Ok(ApiCollectionResponseDto.CreateOk(err.Message, null, 0));
-            }
-
-        }
-        [HttpGet]
-        public async Task<IActionResult> GetAllTenancys([FromQuery] PaginationFilter filter)
-        {
-            try
-            {
-                var (Succeeded, Message, Response) = await _tenancyServices.GetAll(filter);
-
-                if (Succeeded == false)
-                {
-                    return Ok(ApiCollectionResponseDto.CreateOk(Message, null, 0));
-                }
-                else
-                {
-                    //var data = _mapper.Map<List<AdmTenancysDto>>(Data);
-                    //return Ok(ApiCollectionResponseDto.Create(data.Count, data.ToArray()));
-                    return Ok(Response);
-                }
-            }
-            catch (Exception ex)
-            {
-                //return BadRequest(new ApiErrorDto(ModelState));
-                var err = ex;
-                return Ok(ApiCollectionResponseDto.CreateOk(err.Message, null, 0));
-            }
 
         }
 
---
+++ .Net C# Enttity/WebApplicationFlatFile/Controllers/TenancyController.cs
@@ -0,0 +1,71 @@
+﻿using AccountControl.Services.Security;
+using AccountControll.Data.Entities;
+using AccountControll.Dtos.Account;
+using AccountControll.Dtos;
+using AccountControll.Services.Security;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountControll.Controllers
+{
+    //[Authorize]
+    [Route("[controller]/[action]")]
+    public class TenancyController: Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ITenancysServices _tenancysServices;
+        private readonly IJwtGenerator _jwtGenerator;
+
+        public TenancyController(IMapper mapper, ITenancysServices tenancysServices, IJwtGenerator jwtGenerator)
+        {
+            _mapper = mapper;
+            _tenancysServices = tenancysServices;
+            _jwtGenerator = jwtGenerator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTenancys([FromBody] CreateAdmTenancysRequest param)
+        {
+            try
+            {
+                AdmTenancysEntity data = _mapper.Map<AdmTenancysEntity>(param);
+                if (!ModelState.IsValid)
+                    return BadRequest(new ApiErrorDto(ModelState));
+                var (Succeeded, Message) = await _tenancysServices.Create(data);
+                return Ok(ApiCollectionResponseDto.CreateOk(Message, null, 0));
+            }
+            catch (Exception ex)
+            {
+                var err = ex;
+                return Ok(ApiCollectionResponseDto.CreateOk(err.Message, null, 0));
+            }
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllTenancys([FromQuery] PaginationFilter filter)
+        {
+            try
+            {
+                var (Succeeded, Message, Response) = await _tenancysServices.GetAll(filter);
+
+                if (Succeeded == false)
+                {
+                    return Ok(ApiCollectionResponseDto.CreateOk(Message, null, 0));
+                }
+                else
+                {
+                    //var data = _mapper.Map<List<AdmTenancysDto>>(Data);
+                    //return Ok(ApiCollectionResponseDto.Create(data.Count, data.ToArray()));
+                    return Ok(Response);
+                }
+            }
+            catch (Exception ex)
+            {
+                //return BadRequest(new ApiErrorDto(ModelState));
+                var err = ex;
+                return Ok(ApiCollectionResponseDto.CreateOk(err.Message, null, 0));
+            }
+
+        }
+    }
+}
